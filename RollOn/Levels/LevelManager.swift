0bebf205dc706877f0850552d0b7205f701eadca
Jon Evans <Jevans2497@gmail.com>
2020-06-02 16:13:14 -0500

    Working on building level manager

---
+++ RollOn/Levels/LevelManager.swift
@@ -0,0 +1,38 @@
+//
+//  LevelManager.swift
+//  RollOn
+//
+//  Created by Jonathan Evans on 6/2/20.
+//  Copyright © 2020 Jon Evans. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class LevelManager {
+    
+    var levels: [Level] = []
+    var currentLevel = 0
+    
+    init() {
+        makeLevels()
+    }
+    
+    func makeLevels() {
+        levels.append(LevelOne())
+        levels.append(LevelTwo())
+        levels.append(LevelThree())
+        levels.append(LevelFour())
+        levels.append(LevelFive())
+        levels.append(LevelSix())
+        levels.append(LevelSeven())
+        levels.append(LevelEight())
+        levels.append(LevelNine())
+        levels.append(LevelTen())
+    }
+    
+    func nextLevel() -> Level {
+        currentLevel += 1
+        return levels[currentLevel]
+    }
+}
